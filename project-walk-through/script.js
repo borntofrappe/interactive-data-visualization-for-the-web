15766f633049e41f4e1cb860f62897639c9ee7ba
Gabriele Corti <borntofrappe@gmail.com>
2022-11-09 10:33:25 +0100

    transition stacked area charts

--- project-walk-through/script.js
+++ project-walk-through/script.js
@@ -1,226 +1,3 @@
-const highlightNames = () => {
-  clipPath.append("rect").attr("width", width).attr("height", height);
-
-  const groupData = group.append("g").attr("id", "clip-path-highlight-names");
-  const groupAxis = group.append("g");
-
-  const groupLetters = groupData.append("g");
-  const groupOverlay = groupData.append("g");
-  const groupLetter = groupData.append("g");
-
-  groupLetters
-    .selectAll("path")
-    .data(dataStacked)
-    .enter()
-    .append("path")
-    .attr("fill", (_, i) => scaleColor(i % dataStacked.length))
-    .attr("d", area);
-
-  article.style("position", "relative");
-
-  const tooltip = article
-    .append("div")
-    .attr("class", "tooltip")
-    .style("position", "absolute")
-    .style("pointer-events", "none")
-    .style("visibility", "hidden")
-    .style("opacity", "0");
-
-  groupLetters
-    .selectAll("path")
-    .on("pointerenter", function (e, { key }) {
-      d3.select(this).style("filter", "brightness(1.1)");
-
-      tooltip
-        .style("visibility", "visible")
-        .style("opacity", "1")
-        .style("left", `${e.layerX}px`)
-        .style("top", `${e.layerY}px`);
-
-      tooltip
-        .append("p")
-        .html(
-          `<span class="visually-hidden">Letter: </span><strong>${key}</strong>`
-        );
-    })
-    .on("pointermove", (e) => {
-      tooltip.style("left", `${e.layerX}px`).style("top", `${e.layerY}px`);
-    })
-    .on("pointerleave", function () {
-      d3.select(this).style("filter", "brightness(1)");
-
-      tooltip
-        .style("visibility", "hidden")
-        .style("opacity", "0")
-        .selectAll("*")
-        .remove();
-    });
-
-  groupLetters.selectAll("path").on("click", function (e, d) {
-    const letter = d.key;
-
-    const dataStackedKey = d3
-      .stack()
-      .keys(letters)
-      .value((d, key) => {
-        if (key !== letter) return 0;
-
-        return d[key] || 0;
-      })(dataStack);
-
-    scaleY
-      .domain([
-        0,
-        d3.max(dataStackedKey[dataStackedKey.length - 1], ([, m]) => m),
-      ])
-      .nice();
-
-    const transition = d3.transition().duration(750).ease(d3.easeQuadInOut);
-
-    groupAxis.select(".y-axis").transition(transition).call(axisY);
-    groupLetters
-      .selectAll("path")
-      .data(dataStackedKey)
-      .transition(transition)
-      .attr("d", area);
-
-    tooltip
-      .style("visibility", "hidden")
-      .style("opacity", "0")
-      .selectAll("*")
-      .remove();
-
-    groupOverlay.style("pointer-events", "initial");
-
-    const namesLetter = names.filter((name) => name[0] === letter);
-    const dataStackLetter = [...d3.group(dataset, (d) => d.year)].reduce(
-      (acc, [year, data]) => {
-        return [
-          ...acc,
-          {
-            year: timeParse(year),
-            ...data.reduce((a, { name, number }) => {
-              if (name[0] === letter) {
-                a[name] = number;
-              }
-              return a;
-            }, {}),
-          },
-        ];
-      },
-      []
-    );
-
-    const dataStackedEmpty = d3
-      .stack()
-      .keys(namesLetter)
-      .value(() => 0)(dataStackLetter);
-
-    groupLetter
-      .selectAll("path")
-      .data(dataStackedEmpty)
-      .enter()
-      .append("path")
-      .attr("fill", (_, i) => scaleColor(i % dataStackedEmpty.length))
-      .attr("d", area);
-
-    transition.on("end", () => {
-      const transition = d3
-        .transition()
-        .duration(750)
-        .delay(50)
-        .ease(d3.easeQuadInOut);
-
-      const dataStackedLetter = d3
-        .stack()
-        .keys(namesLetter)
-        .value((d, key) => d[key] || 0)(dataStackLetter);
-
-      groupLetter
-        .selectAll("path")
-        .data(dataStackedLetter)
-        .transition(transition)
-        .attr("d", area);
-
-      transition.on("end", () => {
-        groupLetter
-          .selectAll("path")
-          .on("pointerenter", function (e, { key }) {
-            d3.select(this).style("filter", "brightness(1.1)");
-
-            tooltip
-              .style("visibility", "visible")
-              .style("opacity", "1")
-              .style("left", `${e.layerX}px`)
-              .style("top", `${e.layerY}px`);
-
-            tooltip
-              .append("p")
-              .html(
-                `<span class="visually-hidden">Letter: </span><strong>${key}</strong>`
-              );
-          })
-          .on("pointermove", (e) => {
-            tooltip
-              .style("left", `${e.layerX}px`)
-              .style("top", `${e.layerY}px`);
-          })
-          .on("pointerleave", function () {
-            d3.select(this).style("filter", "brightness(1)");
-
-            tooltip
-              .style("visibility", "hidden")
-              .style("opacity", "0")
-              .selectAll("*")
-              .remove();
-          });
-
-        groupOverlay.on(
-          "click",
-          function (e, d) {
-            scaleY
-              .domain([
-                0,
-                d3.max(dataStacked[dataStacked.length - 1], ([, m]) => m),
-              ])
-              .nice();
-
-            const transition = d3
-              .transition()
-              .duration(750)
-              .ease(d3.easeQuadInOut);
-
-            groupAxis.select(".y-axis").transition(transition).call(axisY);
-            groupLetters
-              .selectAll("path")
-              .data(dataStacked)
-              .transition(transition)
-              .attr("d", area);
-
-            groupLetter
-              .selectAll("path")
-              .data(dataStackedEmpty)
-              .transition(transition)
-              .attr("d", area)
-              .remove();
-
-            transition.on("end", () => {
-              groupOverlay.style("pointer-events", "none");
-            });
-          },
-          {
-            once: true,
-          }
-        );
-      });
-    });
-  });
-
-  article
-    .append("p")
-    .html("Click on one of the areas to to highlight the specific curve.");
-};
-
 // REQUIRE A SERVER TO BYPASS CORS ISSUE
 (async () => {
   const data = await d3.csv("data.csv", ({ year, name, number }) => ({
@@ -332,6 +109,8 @@ const highlightNames = () => {
     .range([height, 0])
     .nice();
 
+  const scaleYDomain = scaleY.domain();
+
   const scaleColor = d3.scaleOrdinal(d3.schemeSet2);
 
   const area = d3
@@ -366,7 +145,7 @@ const highlightNames = () => {
 
   const groupLetters = groupData.append("g");
   const groupOverlay = groupData.append("g");
-  const groupLetter = groupData.append("g");
+  const groupNames = groupData.append("g");
 
   groupAxis
     .append("g")
@@ -389,10 +168,12 @@ const highlightNames = () => {
 
   groupOverlay
     .style("pointer-events", "none")
+    .style("cursor", "zoom-out")
     .attr("opacity", "0")
     .append("rect")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("transform", `translate(${-margin.left} ${-margin.top})`)
+    .attr("width", width + (margin.left + margin.right))
+    .attr("height", height + (margin.top + margin.bottom));
 
   groupLetters
     .selectAll("path")
@@ -420,5 +201,164 @@ const highlightNames = () => {
         .style("opacity", "0")
         .selectAll("*")
         .remove();
+    });
+
+  groupLetters
+    .selectAll("path")
+    .style("cursor", "zoom-in")
+    .on("click", function (e, d) {
+      groupLetters.selectAll("path").attr("opacity", "1");
+
+      tooltip
+        .style("visibility", "hidden")
+        .style("opacity", "0")
+        .selectAll("*")
+        .remove();
+
+      groupOverlay.style("pointer-events", "initial");
+
+      const letter = d.key;
+
+      const dataStackedLetter = d3
+        .stack()
+        .keys(letters)
+        .value((d, key) => {
+          if (key !== letter) return 0;
+
+          return d[key] || 0;
+        })(dataStack);
+
+      scaleY
+        .domain([
+          0,
+          d3.max(dataStackedLetter[dataStackedLetter.length - 1], ([, m]) => m),
+        ])
+        .nice();
+
+      const transition = d3.transition().duration(750).ease(d3.easeQuadInOut);
+
+      groupAxis.select(".y-axis").transition(transition).call(axisY);
+      groupLetters
+        .selectAll("path")
+        .data(dataStackedLetter)
+        .transition(transition)
+        .attr("d", area);
+
+      const namesLetter = names.filter((name) => name[0] === letter);
+      const dataStackNames = [...d3.group(data, (d) => d.year)].reduce(
+        (acc, [year, values]) => {
+          return [
+            ...acc,
+            {
+              year: timeParse(year),
+              ...values.reduce((a, { name, number }) => {
+                if (name[0] === letter) {
+                  a[name] = number;
+                }
+                return a;
+              }, {}),
+            },
+          ];
+        },
+        []
+      );
+
+      const dataStackedEmpty = d3
+        .stack()
+        .keys(namesLetter)
+        .value(() => 0)(dataStackNames);
+
+      groupNames
+        .selectAll("path")
+        .data(dataStackedEmpty)
+        .enter()
+        .append("path")
+        .attr("fill", (_, i) => scaleColor(i))
+        .attr("d", area);
+
+      transition.on("end", () => {
+        const transition = d3
+          .transition()
+          .duration(750)
+          .delay(50)
+          .ease(d3.easeQuadInOut);
+
+        const dataStackedNames = d3
+          .stack()
+          .keys(namesLetter)
+          .value((d, key) => d[key] || 0)(dataStackNames);
+
+        groupNames
+          .selectAll("path")
+          .data(dataStackedNames)
+          .transition(transition)
+          .attr("d", area);
+
+        transition.on("end", () => {
+          groupNames
+            .selectAll("path")
+            .on("pointerenter", function (e, { key }) {
+              groupNames.selectAll("path").attr("opacity", "0.5");
+
+              d3.select(this).attr("opacity", 1);
+
+              tooltip
+                .style("visibility", "visible")
+                .style("opacity", "1")
+                .style("left", `${e.layerX}px`)
+                .style("top", `${e.layerY}px`);
+
+              tooltip.append("p").html(`Name: <strong>${key}</strong>`);
+            })
+            .on("pointermove", (e) => {
+              tooltip
+                .style("left", `${e.layerX}px`)
+                .style("top", `${e.layerY}px`);
+            })
+            .on("pointerleave", function () {
+              groupNames.selectAll("path").attr("opacity", "1");
+
+              tooltip
+                .style("visibility", "hidden")
+                .style("opacity", "0")
+                .selectAll("*")
+                .remove();
+            });
+
+          groupOverlay.on(
+            "click",
+            function (e, d) {
+              scaleY.domain(scaleYDomain).nice();
+
+              const transition = d3
+                .transition()
+                .duration(750)
+                .ease(d3.easeQuadInOut);
+
+              groupAxis.select(".y-axis").transition(transition).call(axisY);
+              groupLetters
+                .selectAll("path")
+                .data(dataStacked)
+                .transition(transition)
+                .attr("d", area);
+
+              groupNames
+                .selectAll("path")
+                .data(dataStackedEmpty)
+                .transition(transition)
+                .attr("d", area)
+                .remove();
+
+              transition.on("end", () => {
+                groupOverlay.style("pointer-events", "none");
+              });
+            },
+            {
+              once: true,
+            }
+          );
+          //
+        });
+      });
     });
 })();
